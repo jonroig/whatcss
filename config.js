70d5eaa0d5e2dbd5610af2d2d56222bd640f42da
Jonathan Roig <jroig@Jonathans-MacBook-Pro.local>
2019-01-15 21:15:42 -0700

    progress

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 const config = {
   serverPort: 3000,
+  viewports: [
+    { height: 800, width: 480, deviceScaleFactor: 2 },
+    { height: 100, width: 576, deviceScaleFactor: 2 },
+    { height: 1280, width: 768, deviceScaleFactor: 2 },
+    { height: 1920, width: 992, deviceScaleFactor: 2 },
+    { height: 2560, width: 1200, deviceScaleFactor: 2 },
+  ],
   screenshotsArchive: {
     active: true,
     numberToArchive: 6,
